9143d86c7cd74e3afec9ec30b0560d50c510d1b8
Lucioo04 <luciotesta@gmail.com>
2023-09-24 19:54:24 -0300

    Agregue el archivo escribir.js, que tiene la funcionalidad de escribir el el archivo registros.json

---
+++ escribir.js
@@ -0,0 +1,31 @@
+const fs = require ('fs');
+const path = require('path');
+const ruta = __dirname +"/data/registros.json"
+
+function escribirJson (registro) {
+    if(fs.existsSync(ruta)){
+        fs.readFile(ruta, 'utf8', function readFileCallback(err, data){
+            if (err){
+                console.log(err);
+            } else {
+                var obj = [];
+                if (data.length > 0) {
+                    obj = JSON.parse(data);
+                }
+                obj.push(registro);
+                json = JSON.stringify(obj);
+                fs.writeFileSync(ruta, json, 'utf8');
+        }});
+    } else {
+        var obj = [];
+        obj.push(registro);
+        var json = JSON.stringify(obj);
+        fs.writeFileSync(ruta, json, 'utf8');
+    }
+}
+
+module.exports = {
+    escribirJson
+}
+
+
